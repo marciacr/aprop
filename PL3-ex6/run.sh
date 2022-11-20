e7a8acebdf26d1636f6d1a57b3613944372db312
Gleizi <gleizyalves@hotmail.com>
2022-11-20 07:12:58 -0800

    thread analisis

--- PL3-ex6/run.sh
+++ PL3-ex6/run.sh
@@ -1,13 +1,17 @@
 MSIZES="1000"
 BSIZES="10"
+NUMTHREADS=(2 4 6 8 10)
 
 echo "-----------GCC----------"
 
-for MS in {1..22}; do
-    ./cholesky_gcc 1000 10 4 "cholesky-gcc.csv"
-done
+for NT in ${NUMTHREADS[@]};do 
+    for MS in {1..7}; do
+        ./cholesky_gcc 1000 10 $NT "cholesky-gcc.csv"
+    done
+    
+    echo "-----------LVM----------"
+    for MS in {1..7}; do
+        ./cholesky_lvm 1000 10 $NT "cholesky-lvm.csv"
+    done
 
-echo "-----------LVM----------"
-for MS in {1..22}; do
-    ./cholesky_lvm 1000 10 4 "cholesky-lvm.csv"
 done
